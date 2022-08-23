185dbfde08b631b6fd267cd768c202a393a18c87
Muhammad Taufiq Hidayat <mtfiqh@gmail.com>
2022-08-23 16:47:05 +0700

    improvement error. so if no error it will be nil

--- transport_error.go
+++ transport_error.go
@@ -9,9 +9,9 @@ type Error struct {
 func (e *Error) Error() string {
 	errors := make([]string, 0)
 
-	//for _, ee := range e {
-	//	errors = append(errors, ee.Error())
-	//}
+	for _, ee := range e.errors {
+		errors = append(errors, ee.Error())
+	}
 
 	return strings.Join(errors, ", ")
 }
